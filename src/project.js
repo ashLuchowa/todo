3899a9b5e3ae2759e0962c6bff34adc4d487e599
Ash Luchowa <ashwinluchowa@gmail.com>
2025-02-07 10:45:00 +0800

    Change filename

--- src/project.js
+++ src/project.js
@@ -1,1 +1,26 @@
-export const greeting = 'Hello, Project! This is another test';
+// Empty Array to store default / new projects
+export let projects = [];
+
+// Project Class Template
+export class Project {
+    constructor(title, description, addtaskBtn) {
+        this.title = title;
+        this.description = description;
+        this.addtaskBtn = addtaskBtn;
+    }
+
+    // Push default projects into array
+    static pushDefaultProject() {
+        defaultProjects.forEach(item => {
+            projects.push(item);
+        });
+        console.log(projects);
+    }
+}
+
+// 3 default projects on page load
+const defaultProjects = [
+    new Project('Portfolio 2025', 'Website to feature all my work', 'add-task-btn'),
+    new Project('Cleaning February', 'Cleaning duties around the house', 'add-task-btn'),
+    new Project('Apply Jobs', 'Need to apply jobs', 'add-task-btn'),
+];
